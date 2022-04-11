2e58fda1360ea1fe6347b76d046fa9b69cf5d2da
apragupta <apra@apra.in>
2022-04-10 20:08:47 -0400

    added data formats

--- web-dev-final/src/components/NavigationSidebar/index.js
+++ web-dev-final/src/components/NavigationSidebar/index.js
@@ -1,41 +1,54 @@
 import React from "react"
 
+import {Link} from "react-router-dom";
 
 const NavigationSidebar = ({active = "home"}) =>{
     return(
         <div>
-            <div className="list-group p-0">
-                <a href = "./home" className={`list-group-item list-group-item-action ${active === 'home' ? 'active' : ''}`}>
+        <div className="list-group p-0">
+
+
+                <Link to = "./home" className={`list-group-item list-group-item-action ${active === 'home' ? 'active' : ''}`}>
                     <i className="fa fa-home"></i> <span className="d-xl-inline d-none">Home</span>
-                </a>
-            </div>
+                </Link>
 
 
-            <div className="list-group p-0">
-                <a href = "./profile" className={`list-group-item list-group-item-action ${active === 'profile' ? 'active' : ''}`}>
+
+
+                <Link to= "./profile" className={`list-group-item list-group-item-action ${active === 'profile' ? 'active' : ''}`}>
                     <i className="fa fa-user"></i> <span className="d-xl-inline d-none">Profile</span>
-                </a>
-            </div>
+                </Link>
 
-            <div className="list-group p-0">
-                <a href = "./search" className={`list-group-item list-group-item-action ${active === 'search' ? 'active' : ''}`}>
+
+
+                <Link to= "./search" className={`list-group-item list-group-item-action ${active === 'search' ? 'active' : ''}`}>
                     <i className="fa fa-search"></i> <span className="d-xl-inline d-none">Search Games</span>
-                </a>
-            </div>
+                </Link>
+
 
 
             {/*Note - this should only be there for loggedIn users*/}
-            <div className="list-group p-0">
-                <a href = "./edit-profile" className={`list-group-item list-group-item-action ${active === 'edit-profile' ? 'active' : ''}`}>
-                    <i className="fas fa-pencil-alt"></i> <span className="d-xl-inline d-none">Edit Profile</span>
-                </a>
-            </div>
 
-            <div className="list-group p-0">
-                <a href = "./privacy" className={`list-group-item list-group-item-action ${active === 'privacy' ? 'active' : ''}`}>
+                <Link to = "./edit-profile" className={`list-group-item list-group-item-action ${active === 'edit-profile' ? 'active' : ''}`}>
+                    <i className="fas fa-pencil-alt"></i> <span className="d-xl-inline d-none">Edit Profile</span>
+                </Link>
+
+
+
+                <Link to = "./privacy" className={`list-group-item list-group-item-action ${active === 'privacy' ? 'active' : ''}`}>
                     <i className="fas fa-user-secret"></i> <span className="d-xl-inline d-none">Privacy Policy</span>
-                </a>
-            </div>
+                </Link>
+        </div>
+
+            {/*Note - this should only be there for loggedIn users*/}
+
+        <div className="py-2 ">
+            <button className="  btn btn-primary btn-block rounded-pill w-100 h-auto mx-auto px-0">
+                Login/Register
+
+            </button>
+        </div>
+
 
 
         </div>
---
+++ web-dev-final/src/components/PostList/PostListItem.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+const PostListItem = ({post}) => {
+
+}
