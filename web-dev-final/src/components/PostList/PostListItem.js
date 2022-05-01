17f94912bb3621e7763487695002f3ff7e8480b1
apragupta <apra@apra.in>
2022-04-30 22:57:43 -0400

    added links to profile and game in post, need to refactor with new game and post endpoint

--- web-dev-final/src/components/PostList/PostListItem.js
+++ web-dev-final/src/components/PostList/PostListItem.js
@@ -3,6 +3,7 @@ import {useDispatch} from "react-redux";
 import PostStats from "./PostStats.js";
 import {deletePost} from "../../actions/posts-actions";
 import "./post-list.css"
+import {Link} from "react-router-dom";
 
 const PostListItem = ({post}) => {
     const dispatch = useDispatch();
@@ -11,16 +12,21 @@ const PostListItem = ({post}) => {
     return(
         <div className="list-group-item d-flex my-2  wd-inherit-bkg wd-post-list">
             <div className="col-lg-2 col-sm-3 col-4  h-auto  p-0 ratio-1x1 pe-4  pt-1" >
-
+                <Link to={`../profile/${post.postedBy._id}`} id="profile-link">
                 <img src={post.postedBy.avatar_image} className = "img-fluid   rounded-circle wd-avatar-border " />
+                </Link>
             </div>
 
             <div className="col-lg-7 col-sm-6 col-5 mr-auto p-0 ">
                 <div className="d-flex justify-content-between ">
                     <div>
+
                         <div className="d-flex justify-content-start mb-1">
+                            <Link to={`../profile/${post.postedBy._id}`} id="profile-link">
                             <div className="fw-bold me-1 "> {post.postedBy && post.postedBy.name}</div>
+                            </Link>
                             <div className="me-1 wd-username-color" >@{post.postedBy.username && post.postedBy.username}</div>
+
                         </div>
 
                         <p><strong>{post.title}</strong></p>
@@ -44,10 +50,14 @@ const PostListItem = ({post}) => {
 
 
             </div>
+
             <div className="col-lg-3 col-sm-3 d-sm-block d-none ms-1  p-0 text-center align-self-center wd-post-game">
+                <Link to={`../game/${post.game._id}`} id="game-link">
                 <img src={post.game.header_image} className = "img-fluid w-auto h-auto wd-post-game-image" />
                 <h6 className="my-auto ">{post.game.name}</h6>
+                </Link>
             </div>
+
             {/*Todo - image/embedding-*/}
 
 
