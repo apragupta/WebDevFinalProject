08fbb2db56cf80ef18f55e230dfc22e001e42bb2
Dustin <yangdustin@outlook.com>
2022-05-01 13:02:58 -0400

    likes/dislikes mutually exclusive on frontend

--- web-dev-final/src/components/PostList/PostStats.js
+++ web-dev-final/src/components/PostList/PostStats.js
@@ -14,7 +14,7 @@ const PostStats = ({post}) => {
         }
 
     }
-    const calcDisLikes = (post) => {
+    const calcDislikes = (post) => {
         //calculates the new number of likes depending on whether the tweet is already liked
         if(post.disliked){
             return post.stats.dislikes -1
@@ -26,23 +26,68 @@ const PostStats = ({post}) => {
     }
 
 
-    const handleLike = () => { updatePost(dispatch, {
-        ...post, stats: {...post.stats,
-                    likes: calcLikes(post)},
-                    liked: !post.liked})}
-    const handleDisLike = () => { updatePost(dispatch, {
-        ...post, stats: {...post.stats,
-            dislikes: calcDisLikes(post)},
-        disliked: !post.disliked})}
+    const handleLike = () => {
+        if (post.disliked) {
+            updatePost(dispatch, {
+                ...post,
+                stats: {
+                    ...post.stats,
+                    likes: post.stats.likes + 1,
+                    dislikes: post.stats.dislikes - 1
+                },
+                liked: !post.liked,
+                disliked: !post.disliked
+            });
+        } else {
+            updatePost(dispatch, {
+                ...post,
+                stats: {
+                    ...post.stats,
+                    likes: calcLikes(post)
+                },
+                liked: !post.liked
+            });
+        }
+    }
 
-    const handleBookmark = () => { updatePost(dispatch,
-        {...post,  bookmarked: !post.bookmarked})}
+    const handleDislike = () => {
+        // if user liked the post, dislike should dec likes by 1 and inc dislikes by 1
+        // if otherwise inc dislikes by 1 if not disliked and dec by 1 if already disliked
+        if (post.liked) {
+            updatePost(dispatch, {
+                ...post,
+                stats: {
+                    ...post.stats,
+                    dislikes: post.stats.dislikes + 1,
+                    likes: post.stats.likes - 1
+                },
+                disliked: !post.disliked,
+                liked: !post.liked
+            });
+        } else {
+            updatePost(dispatch, {
+                ...post,
+                stats: {
+                    ...post.stats,
+                    dislikes: calcDislikes(post)
+                },
+                disliked: !post.disliked
+            });
+        }
+    }
+
+    const handleBookmark = () => {
+        updatePost(dispatch, {
+            ...post,
+            bookmarked: !post.bookmarked
+        });
+    }
 
 
     return (
         <div className="d-flex justify-content-around">
             <i className="far fa-comment mx-1"> { post.stats && post.stats.comments} </i>
-            <span onClick={handleDisLike} className="align-text-top">
+            <span onClick={handleDislike} className="align-text-top">
                   {
                       post.disliked &&
                       <i className="fas fa-thumbs-down d-flex float-start"
